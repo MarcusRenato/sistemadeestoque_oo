52ebb8445929a9c7be1587da721aac2378e318e1
Marcus Renato <marcusrenato@live.com>
2019-08-30 16:23:27 -0300

    Adicionando excecao ao construtor da classe usuario e identando codigo.

--- classes/usuarios.class.php
+++ classes/usuarios.class.php
@@ -1,12 +1,19 @@
 <?php
-class Usuarios {
+class Usuarios
+{
     private $pdo;
 
-    public function __construct() {
-        $this->pdo = new PDO("mysql:dbname=estoque;host=localhost", "root", "");
+    public function __construct()
+    {
+        try {
+            $this->pdo = new PDO("mysql:dbname=estoque;host=localhost", "root", "");
+        } catch (PDOException $e) {
+            echo "Falhou: " . $e->getMessage();
+        }
     }
 
-    public function existeUsuario($email, $senha) {
+    public function existeUsuario($email, $senha)
+    {
         $sql = $this->pdo->prepare("SELECT * FROM usuarios WHERE email = :email AND senha = :senha");
         $sql->bindValue(":email", $email);
         $sql->bindValue(":senha", $senha);
